8345020fd24203e65e99e95c6e18c5660bb6e21e
WindzJC <windellejcr@gmail.com>
2023-02-22 19:56:32 +0800

    External Storage - Vowel and Consonant Count

--- app/src/main/java/com/example/externalstorage/MainActivity.java
+++ app/src/main/java/com/example/externalstorage/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.externalstorage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -18,51 +17,44 @@ import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView promptTextView;
     private EditText inputEditText;
     private TextView vowelCountTextView;
     private TextView consonantCountTextView;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        promptTextView = findViewById(R.id.prompt_textview);
         inputEditText = findViewById(R.id.input_edittext);
         vowelCountTextView = findViewById(R.id.vowel_count_textview);
         consonantCountTextView = findViewById(R.id.consonant_count_textview);
 
         Button countButton = findViewById(R.id.count_button);
-        countButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String input = inputEditText.getText().toString().toLowerCase();
-                int vowelCount = 0;
-                int consonantCount = 0;
+        countButton.setOnClickListener(v -> {
+            String input = inputEditText.getText().toString().toLowerCase();
+            int vowelCount = 0;
+            int consonantCount = 0;
 
-                for (int i = 0; i < input.length(); i++) {
-                    char c = input.charAt(i);
-                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                        vowelCount++;
-                    } else if (c >= 'a' && c <= 'z') {
-                        consonantCount++;
-                    }
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vowelCount++;
+                } else if (c >= 'a' && c <= 'z') {
+                    consonantCount++;
                 }
-
-                vowelCountTextView.setText("Vowels: " + vowelCount);
-                consonantCountTextView.setText("Consonants: " + consonantCount);
             }
+
+            vowelCountTextView.setText("Vowels: " + vowelCount);
+            consonantCountTextView.setText("Consonants: " + consonantCount);
         });
 
         Button saveButton = findViewById(R.id.save_button);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String input = inputEditText.getText().toString();
-                saveToSDCard(input);
-                Toast.makeText(MainActivity.this, "Input saved to SD card", Toast.LENGTH_SHORT).show();
-            }
+        saveButton.setOnClickListener(v -> {
+            String input = inputEditText.getText().toString();
+            saveToSDCard(input);
+            Toast.makeText(MainActivity.this, "Input saved to SD card", Toast.LENGTH_SHORT).show();
         });
     }
 
